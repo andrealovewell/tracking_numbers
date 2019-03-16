66fdd1cdff472af115a0419cccb5f78c4b140cbe
Andrea Lovewell <andy.lovewell@gmail.com>
2019-03-16 06:03:23 +0000

    first commit

---
+++ lib/tracking_numbers.rb
@@ -0,0 +1,6 @@
+require "tracking_numbers/version"
+
+module TrackingNumbers
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/tracking_numbers/version.rb
@@ -0,0 +1,3 @@
+module TrackingNumbers
+  VERSION = "0.1.0"
+end
