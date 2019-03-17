7c989c2dd0027e70b363a4add07c89e83c422574
Andrea Lovewell <andy.lovewell@gmail.com>
2019-03-17 04:35:17 +0000

    modified tracking_numbers

--- lib/tracking_numbers.rb
+++ lib/tracking_numbers.rb
@@ -1,4 +1,12 @@
 require "tracking_numbers/version"
+require "tracking_numbers/version"
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
+require_relative "./tracking_numbers/version"
+require_relative './tracking_numbers/cli'
+require_relative './tracking_numbers/tracking'
 
 module TrackingNumbers
   class Error < StandardError; end
